f153b4d43e35908f89db6191f1f13a09342497b8
mkons <mikykonst66@gmail.com>
2019-05-15 18:40:44 +0300

    Apollo GraphQl was added 0.1

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -14,7 +14,7 @@ export class MainPageComponent implements OnInit {
   constructor(private apollo: Apollo, private service: MockserviceService) { }
 
   ngOnInit() {
-    this.service.getAppoloData(this.apollo).valueChanges.subscribe(result => {
+    this.service.getAppoloData(this.apollo).valueChanges.subscribe((result: any) => {
       this.tours = result.data && result.data.tours;
       this.loading = result.loading;
       this.error = result.error;
